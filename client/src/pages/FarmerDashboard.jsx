10e554943052e8748406253790f4e1810690750e
Mohd Akif <makif0456@gmail.com>
2026-09-10 17:09:20 +0530

    Add MarketIntelligence component to FarmerDashboard

--- client/src/pages/FarmerDashboard.jsx
+++ client/src/pages/FarmerDashboard.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import FarmerHome from '../components/farmer/FarmerHome';
+import MarketIntelligence from '../components/farmer/MarketIntelligence';
 
 export default function FarmerDashboard() {
   return (
-    <div className="py-2 sm:py-4">
+    <div className="py-2 sm:py-4 space-y-6">
       <FarmerHome />
+
+      <MarketIntelligence />
     </div>
   );
 }
